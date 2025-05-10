f7e92666a223140242d8886783d894e2c8137983
Anant <anantsinghraghuvanshi1@gmail.com>
2025-05-10 23:43:00 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PromiseComponent } from './promise/promise.component';
+import { ObservablesComponent } from './observables/observables.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'promise', pathMatch: 'full' },
+  { path: 'promise', component: PromiseComponent },
+  { path: 'observables', component: ObservablesComponent }
+];
---
+++ src/app/promise/promise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promise',
+  imports: [],
+  templateUrl: './promise.component.html',
+  styleUrl: './promise.component.css'
+})
+export class PromiseComponent {
+
+}
